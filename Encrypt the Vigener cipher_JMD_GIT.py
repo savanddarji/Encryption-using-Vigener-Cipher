8fa58abd4440bf178a5f606e8e99af76f3832d33
Sam <savanddarji@gmail.com>
2017-02-12 19:34:55 -0500

    Update Encrypt the Vigener cipher_JMD_GIT.py

--- Encrypt the Vigener cipher_JMD_GIT.py
+++ Encrypt the Vigener cipher_JMD_GIT.py
@@ -6,7 +6,6 @@ def shift(l1,n1): # for left shift operation
     return l1[n1:] + l1[:n1]
 num = dict(zip(string.ascii_lowercase,xrange(0,26)))# for reverse mapping: numbers to letter
 num1= dict(zip(xrange(0,26),string.ascii_lowercase))
-#A = [0.08167,0.01492,0.02782,0.04253,0.12702,0.02228,0.02015,0.06094,0.06966,0.00153,0.00772,0.04025,0.02406,0.06749,0.07507,0.01929,0.00095,0.05987,0.06327,0.09056,0.02758,0.00978,0.0236,0.0015,0.01974,0.00074]
 a1=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 a=raw_input('Please enter your plain text:').lower()
 key=raw_input('Enter the encryption key:').lower()
@@ -17,21 +16,16 @@ v1=0
 while v1<L:
     for i2 in xrange(v1,len(a),L):
         z[v1].append(a[i2])
-#    print ''.join(z[v1])
     v1+=1
 key=list(key)
 v1=0
 z1=[[]for x1 in xrange(0,L)]#for char in key:
-print L
 while v1<L:
-#    print 'at 1:',v1
     for char in z[v1]:
         nbr=(num[char]+num[key[v1]])
         nbr=num1[nbr%26]
         z1[v1].append(nbr)
-    print ''.join(z1[v1])
     v1+=1
-#    print v1
 ###################### recovering the L parts into deciphered form ################
 v1=0
 var=0
